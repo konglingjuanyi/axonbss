2ff82bff78d971ea109cf0e2a83c339727223d96
zlianhua@msn.com <zhanglh@10.11.16.62>
2016-11-14 11:16:42 +0800

    activation

---
+++ aibss-service-activity-api/src/main/java/com/ai/bss/api/activation/ServiceActivationApiAPP.java
@@ -0,0 +1,9 @@
+package com.ai.bss.api.activation;
+
+import org.springframework.boot.SpringApplication;
+public class ServiceActivationApiAPP {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ServiceActivationApiAPP.class, args);
+	}
+
+}
